68f19594d36982dd5806fdca44bb39d2f9145567
Endagegnehu <endukokam@gmail.com>
2020-08-01 21:03:35 +0300

    form ui 🎉

--- lib/presentation/core/app_widget.dart
+++ lib/presentation/core/app_widget.dart
@@ -5,7 +5,18 @@ class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Note',
       home: SignInPage(),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.light().copyWith(
+        primaryColor: Colors.green[800],
+        accentColor: Colors.blueAccent,
+        inputDecorationTheme: InputDecorationTheme(
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+        ),
+      ),
     );
   }
 }
--- lib/presentation/sign_in/widget/sign_in_form.dart
+++ lib/presentation/sign_in/widget/sign_in_form.dart
@@ -1,8 +1,83 @@
+import 'package:firebase_todo/application/auth/sign_in_form/sing_in_form_bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class SignInForm extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return BlocConsumer<SignInFormBloc, SignInFormState>(
+      listener: (context, state) {},
+      builder: (context, state) {
+        return Form(
+          autovalidate: state.showErrorMessages,
+          child: ListView(
+            children: <Widget>[
+              const Text(
+                '📝',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 160,
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              TextFormField(
+                decoration: const InputDecoration(
+                  prefixIcon: Icon(
+                    Icons.email,
+                  ),
+                  labelText: 'Emial',
+                ),
+                autocorrect: false,
+                onChanged: (value) => context
+                    .bloc<SignInFormBloc>()
+                    .add(SignInFormEvent.emailChanged(value)),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              TextFormField(
+                decoration: const InputDecoration(
+                  prefixIcon: Icon(
+                    Icons.lock,
+                  ),
+                  labelText: 'Password',
+                ),
+                autocorrect: false,
+                obscureText: true,
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Row(
+                children: <Widget>[
+                  Expanded(
+                    child: FlatButton(
+                      onPressed: () {},
+                      child: const Text('SIGN IN'),
+                    ),
+                  ),
+                  Expanded(
+                    child: FlatButton(
+                      onPressed: () {},
+                      child: const Text('REGIDTER'),
+                    ),
+                  ),
+                ],
+              ),
+              RaisedButton(
+                onPressed: () {},
+                color: Colors.blue,
+                child: const Text(
+                  'SIGN IN WITH GOOGLE',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+    );
   }
 }
